abb19ccedbac93770d635cb7a8e29ff303bab37a
JHowell45 <jhowell45@googlemail.com>
2020-05-11 21:15:56 +0100

    Finished ep 8.

--- app/src/routes/web.php
+++ app/src/routes/web.php
@@ -28,29 +28,32 @@
 // });
 
 
-Route::get('/', function () {
-    return view('test', [
-      'name' => request('name')
-    ]);
-});
+// Route::get('/', function () {
+//     return view('test', [
+//       'name' => request('name')
+//     ]);
+// });
 
 
 // Route::get('/posts/{post}', function () {
 //   return view('post');
 // });
 
+//  Episode 7 -- Using wildcards
+// Route::get('/posts/{post}', function ($post) {
+//   $posts = [
+//     'my-first-post' => 'Hello this is my first blog post!',
+//     'my-second-post' => 'Now I am getting the hand of this blogging thing'
+//   ];
 
-Route::get('/posts/{post}', function ($post) {
-  $posts = [
-    'my-first-post' => 'Hello this is my first blog post!',
-    'my-second-post' => 'Now I am getting the hand of this blogging thing'
-  ];
+//   if (! array_key_exists($post, $posts)) {
+//     abort(404, 'Sorry, post was not found!'); 
+//   }
 
-  if (! array_key_exists($post, $posts)) {
-    abort(404, 'Sorry, post was not found!'); 
-  }
+//   return view('post', [
+//     'post' => $posts[$post]
+//   ]);
+// });
 
-  return view('post', [
-    'post' => $posts[$post]
-  ]);
-});
+// Episode 8 -- Using controllers
+Route::get('posts/{post}', 'PostsController@show');
