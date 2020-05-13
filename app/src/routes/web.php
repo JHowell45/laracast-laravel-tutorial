d841b257fecd68d4b964dd34f4c61b2e60f34af5
JHowell45 <jhowell45@googlemail.com>
2020-05-13 19:16:10 +0100

    Cleared out old routes.

--- app/src/routes/web.php
+++ app/src/routes/web.php
@@ -39,21 +39,5 @@
 //   return view('post');
 // });
 
-//  Episode 7 -- Using wildcards
-// Route::get('/posts/{post}', function ($post) {
-//   $posts = [
-//     'my-first-post' => 'Hello this is my first blog post!',
-//     'my-second-post' => 'Now I am getting the hand of this blogging thing'
-//   ];
-
-//   if (! array_key_exists($post, $posts)) {
-//     abort(404, 'Sorry, post was not found!'); 
-//   }
-
-//   return view('post', [
-//     'post' => $posts[$post]
-//   ]);
-// });
-
 // Episode 8 -- Using controllers
 Route::get('posts/{post}', 'PostsController@show');
