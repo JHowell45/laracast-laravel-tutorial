8bb80a6035b7199c38a595a9a42807b9fbe54761
JHowell45 <jhowell45@googlemail.com>
2020-05-14 13:53:24 +0100

    Formatted the files.

--- app/src/app/Http/Controllers/PostsController.php
+++ app/src/app/Http/Controllers/PostsController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use DB;
 use App\Post;
 
 class PostsController extends Controller
 {
-	public function show($slug) 
+	public function show($slug)
 	{
 		return view('post', [
 			'post' => Post::where('slug', $slug)->firstOrFail()
