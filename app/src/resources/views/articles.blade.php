051005a5f8481417ffb3eb690248f70501066ec6
JHowell45 <jhowell45@googlemail.com>
2020-05-26 21:25:57 +0100

    added a href to the titles to allow for linking to the specific page.

--- app/src/resources/views/articles.blade.php
+++ app/src/resources/views/articles.blade.php
@@ -13,7 +13,9 @@
 			<ul class="style1">
 				@foreach ($articles as $article)
 				<li class="first">
-					<h3>{{ $article->title }}</h3>
+					<h3>
+						<a href="/articles/{{ $article->id }}">{{ $article->title }}</a>
+					</h3>
 					<p><img src="images/banner.jpg" alt="" class="image image-full" /> </p>
 					<p>{{ $article->excerpt }}</p>
 				</li>
