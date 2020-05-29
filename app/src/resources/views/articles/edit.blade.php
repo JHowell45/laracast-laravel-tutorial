85fcac781e0632429183830e859839342914d885
JHowell45 <jhowell45@googlemail.com>
2020-05-29 01:34:37 +0100

    Updated page to load the Article data into the areas.

--- app/src/resources/views/articles/edit.blade.php
+++ app/src/resources/views/articles/edit.blade.php
@@ -7,16 +7,17 @@
 @section('content')
 	<div id="wrapper">
 		<div id="page"class="Container">
-			<h1 class="heading has-text-weight-bold is-size-4">New Article</h1>
+			<h1 class="heading has-text-weight-bold is-size-4">Update Article</h1>
 
-			<form method="POST" action="/articles">
+		<form method="POST" action="/articles/{{ $article->id }}">
 				@csrf
+				@method('PUT')
 
 				<div class="field">
 					<label class="label" for="title">Title</label>
 
 					<div class="control">
-						<input class="input" type="text" name="title" id="title">
+					<input class="input" type="text" name="title" id="title" value="{{ $article->title }}">
 					</div>
 				</div>
 
@@ -24,7 +25,7 @@
 					<label class="label" for="excerpt">Excerpt</label>
 
 					<div class="control">
-						<textarea class="textarea" type="text" name="excerpt" id="excerpt"></textarea>
+						<textarea class="textarea" type="text" name="excerpt" id="excerpt">{{ $article->excerpt }}</textarea>
 					</div>
 				</div>
 
@@ -32,7 +33,7 @@
 					<label class="label" for="body">Body</label>
 
 					<div class="control">
-						<textarea class="textarea" type="text" name="body" id="body"></textarea>
+						<textarea class="textarea" type="text" name="body" id="body">{{ $article->body }}</textarea>
 					</div>
 				</div>
 
