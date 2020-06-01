494d25e2f66555a05d9124d2ced24e17203af25c
JHowell45 <jhowell45@googlemail.com>
2020-06-01 18:01:31 +0100

    Handled passing of data to the Model created using the Article Model.
    Means we have access to filling the new Model using requests.

--- app/src/app/Article.php
+++ app/src/app/Article.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+	// protected $fillable = ['title', 'excerpt', 'body'];
+	protected $guarded = []; // as long as you're safe and don't pass everything from request!
 }
