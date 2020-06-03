50fe136bdfcfa86782d33af19a81371d530e35b0
JHowell45 <jhowell45@googlemail.com>
2020-06-03 13:21:54 +0100

    Updated the Model to include a path to the named route.

--- app/src/app/Article.php
+++ app/src/app/Article.php
@@ -8,4 +8,9 @@ class Article extends Model
 {
 	// protected $fillable = ['title', 'excerpt', 'body'];
 	protected $guarded = []; // as long as you're safe and don't pass everything from request!
+
+	public function path()
+	{
+		return route('articles.show', $this)
+	}
 }
