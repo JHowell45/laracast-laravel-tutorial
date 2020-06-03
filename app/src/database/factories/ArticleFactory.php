d23862a847467e4e5c670ec587649b93c712c6c0
JHowell45 <jhowell45@googlemail.com>
2020-06-03 16:10:19 +0100

    Built up factory class for Article.

--- app/src/database/factories/ArticleFactory.php
+++ app/src/database/factories/ArticleFactory.php
@@ -6,7 +6,10 @@ use App\Article;
 use Faker\Generator as Faker;
 
 $factory->define(Article::class, function (Faker $faker) {
-    return [
-        //
-    ];
+	return [
+		'user_id' => factory(\App\User::class),
+		'title' => $faker->sentence,
+		'excerpt' => $faker->sentence,
+		'body' => $faker->paragraph,
+	];
 });
