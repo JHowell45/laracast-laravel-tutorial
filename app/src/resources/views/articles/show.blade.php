bf39a7dbe5935033ae335d6efe7d364309be8da5
JHowell45 <jhowell45@googlemail.com>
2020-06-04 16:08:58 +0100

    Added tags to an article.

--- app/src/resources/views/articles/show.blade.php
+++ app/src/resources/views/articles/show.blade.php
@@ -10,6 +10,12 @@
 			<p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
 			
 			{{ $article->body }}
+
+			<p style="margin-top: 1em">
+				@foreach ($article->tags as $tag)
+					<a href="/articles?tag={{ $tag->name }}">{{ $tag->name }}</a>
+				@endforeach
+			</p>
 		</div>
 	</div>
 </div>
