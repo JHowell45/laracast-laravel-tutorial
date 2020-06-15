276e54292b6a7e5eaebf24207ed5eee326fad840
JHowell45 <jhowell45@googlemail.com>
2020-06-15 16:42:53 +0100

    Updated version.

--- get_laravel.sh
+++ get_laravel.sh
@@ -1,7 +1,7 @@
 
 if [ -z "$1" ]
   then
-    VERSION=0.0.1
+    VERSION=0.0.2
   else
     VERSION=$1
 fi
@@ -9,4 +9,4 @@ echo "Version number passed: $VERSION"
 
 curl -L https://github.com/JHowell45/laravel-docker-template/archive/$VERSION.tar.gz | tar xz
 
-mv laravel-docker-template-$VERSION app
+mv laravel-docker-template-$VERSION laravel-vue-app
